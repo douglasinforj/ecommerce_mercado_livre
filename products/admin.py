8ae2ef2264a6ac0bb6cf4495e5ce1ba5a84f8ab3
Douglas Silva <douglasitpro@gmail.com>
2026-03-18 18:00:52 -0300

    Registrando Product no admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Brand
+from .models import Category, Brand, Product, ProductImage, ProductVariant
+from django.utils.html import format_html
 
 
 
@@ -31,6 +32,65 @@ class BrandAdmin(admin.ModelAdmin):
 
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+    fields = ['image', 'alt_text', 'is_main', 'order']
 
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'get_price_display', 'stock', 'is_available', 'is_featured', 'created_at']
+    list_filter = ['category', 'brand', 'is_available', 'is_featured']
+    search_fields = ['name', 'sku', 'short_description']
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ['views_count', 'created_at', 'updated_at', 'get_product_images']
+    
+    fieldsets = (
+        ('Informações Básicas', {
+            'fields': ('category', 'brand', 'name', 'slug', 'sku')
+        }),
+        ('Descrição', {
+            'fields': ('short_description', 'full_description', 'specifications')
+        }),
+        ('Preços', {
+            'fields': ('price', 'discount_price')
+        }),
+        ('Estoque', {
+            'fields': ('stock', 'is_available', 'is_featured')
+        }),
+        ('SEO', {
+            'fields': ('meta_title', 'meta_description', 'meta_keywords'),
+            'classes': ('collapse',)
+        }),
+        ('Estatísticas', {
+            'fields': ('views_count', 'created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    inlines = [ProductImageInline, ProductVariantInline]
+    
+    def get_price_display(self, obj):
+        if obj.discount_price:
+            return format_html(
+                '<span style="text-decoration: line-through; color: #999;">R$ {}</span> '
+                '<span style="color: green; font-weight: bold;">R$ {}</span>',
+                obj.price, obj.discount_price
+            )
+        return f'R$ {obj.price}'
+    get_price_display.short_description = 'Preço'
+    
+    def get_product_images(self, obj):
+        images = obj.images.all()
+        html = ''
+        for img in images[:5]:
+            html += f'<img src="{img.image.url}" style="max-height: 50px; margin-right: 5px;" />'
+        return format_html(html)
+    get_product_images.short_description = 'Imagens'
 
 
