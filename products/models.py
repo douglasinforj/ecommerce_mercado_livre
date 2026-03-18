08e52adf5a5bc7e8fe9c4572a614fe309fe124f7
Douglas Silva <douglasitpro@gmail.com>
2026-03-18 12:51:03 -0300

    model products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Slug', unique=True)
+    description = models.TextField('Descrição', blank=True)
+    image = models.ImageField('Imagem', upload_to='categories/', blank=True, null=True)
+    is_active = models.BooleanField('Ativo', default=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("products:category", args=[self.slug])
+    
