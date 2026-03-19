5656814664b4958c2b2601b7aaa0a42206d6dafa
Douglas Silva <douglasitpro@gmail.com>
2026-03-19 10:59:49 -0300

    Views product_list

--- products/views.py
+++ products/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from .models import Product, Category
 
+from .filters import ProductFilter
+from django.core.paginator import Paginator
+
+
 
 def home(request):
     #Pagina inicial com destaques e novidades
@@ -15,6 +19,31 @@ def home(request):
         'categories': categories,
     }
     return render(request, 'products/home.html', context)
+
+
+
+def product_list(request):
+    """Lista todos os produtos com filtros"""
+    products = Product.objects.filter(is_available=True)
+    
+    # Aplicar filtros
+    product_filter = ProductFilter(request.GET, queryset=products)
+    products = product_filter.qs
+    
+    # Paginação
+    paginator = Paginator(products, 12)  # 12 produtos por página
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+    
+    # Categorias para o menu
+    categories = Category.objects.filter(is_active=True)
+    
+    context = {
+        'products': products,
+        'filter': product_filter,
+        'categories': categories,
+    }
+    return render(request, 'products/product_list.html', context)
 
 
 
